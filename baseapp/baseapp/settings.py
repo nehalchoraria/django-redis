2e85b5ded2b07221515d67713a8d8516ddff8661
nehalchoraria <nehalchoraria09@gmail.com>
2019-12-17 17:23:37 +0530

    fixed wsgi

--- baseapp/baseapp/settings.py
+++ baseapp/baseapp/settings.py
@@ -137,6 +137,8 @@ USE_L10N = True
 
 USE_TZ = True
 
+WSGI_APPLICATION = 'baseapp.wsgi.application'
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
